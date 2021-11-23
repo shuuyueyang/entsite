02de2a220be4573f620d671855094283c79aa651
shuuyueyang <shuuyueyang@hotmail.com>
2021-11-23 22:54:31 +0800

    show 类别和新闻 in news page

--- news/views.py
+++ news/views.py
@@ -4,10 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import News
+from .models import Category
 # ...
 def index(request):
+    categories = Category.objects.all()
+    news = News.objects.all()
     context = {
         'msg': 'hello news home',
+        'categories':categories,
+        'news': news
     }
     return render(request, 'news/index.html', context)
 def detail(request, news_id):
